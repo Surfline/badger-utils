999b925f79568f9a321c5af5702d2895b5139111
jawang35 <jwang@surfline.com>
2019-04-26 11:02:59 -0700

    Update README.md and example to use a more complex key

--- writer_test.go
+++ writer_test.go
@@ -29,15 +29,15 @@ field31,field32,field33`)
 	require.Nil(t, err)
 	require.Equal(t, 3, len(writtenSampleRecords))
 	require.EqualValues(t, writtenSampleRecords[0], sampleRecord{
-		Key: []string{"field11", "field12", "field13"},
+		Key:   []string{"field11", "field12", "field13"},
 		Value: sampleValues{"field11", "field12", "field13"},
 	})
 	require.EqualValues(t, writtenSampleRecords[1], sampleRecord{
-		Key: []string{"field21", "field22", "field23"},
+		Key:   []string{"field21", "field22", "field23"},
 		Value: sampleValues{"field21", "field22", "field23"},
 	})
 	require.EqualValues(t, writtenSampleRecords[2], sampleRecord{
-		Key: []string{"field31", "field32", "field33"},
+		Key:   []string{"field31", "field32", "field33"},
 		Value: sampleValues{"field31", "field32", "field33"},
 	})
 }
